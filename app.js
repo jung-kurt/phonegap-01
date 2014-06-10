125e6832d458f5b3d4d6f24499e8dffbfd9b68ab
Kurt Jung <kurt.w.jung@gmail.com>
2014-06-10 14:09:06 -0400

    Various app resources: main page, jquery, etc

---
+++ app.js
@@ -0,0 +1,26 @@
+﻿jQuery(function(jq) {
+
+  'use strict';
+
+  var lcl = {};
+
+  lcl.pageSwitch = function(pageStr) {
+    var pageOn, pageOff;
+    pageOn = jq('[data-kj-page="' + pageStr + '"]');
+    if (pageOn.length === 1) {
+      // Turn on target page first, then turn off page or pages being left
+      pageOff = jq('[data-kj-page]').filter(":visible");
+      pageOn.show();
+      pageOff.hide();
+    }
+  };
+
+  jq('button').addTouch();
+  jq('[data-kj-page-go]').addTouch();
+  jq('input').addTouch();
+
+  jq('body').on('click', '[data-kj-page-go]', function(evt) {
+    lcl.pageSwitch(jq(evt.target).attr('data-kj-page-go'));
+  });
+
+});
